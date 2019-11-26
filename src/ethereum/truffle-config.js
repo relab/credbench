ef8d2a75ff1eb015d924977e987731436a62da6a
Rodrigo Q. Saramago <rodrigoqsaramago@gmail.com>
2019-11-26 18:30:19 +0100

    Update truffle config

--- src/ethereum/truffle-config.js
+++ src/ethereum/truffle-config.js
@@ -4,26 +4,33 @@ require('chai/register-should');
 
 module.exports = {
     networks: {
-        development: {
-            host: '127.0.0.1',
+        development: { // local test net
+            host: "127.0.0.1",
             port: 8545,
-            network_id: '42' // eslint-disable-line camelcase
+            network_id: "*" // eslint-disable-line camelcase
         },
-        ganache: {
-            host: '127.0.0.1',
+        ganache: { // ganache-cli
+            host: "127.0.0.1",
             port: 7545,
-            network_id: '5777' // eslint-disable-line camelcase
-        }
+            network_id: "5777" // eslint-disable-line camelcase
+        },
+        develop: { // truffle development
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "*", // eslint-disable-line camelcase
+            accounts: 5,
+            defaultEtherBalance: 50
+        },
     },
 
     mocha: {
-    // timeout: 100000,
+        // timeout: 100000,
         useColors: true
     },
 
     compilers: {
         solc: {
-            version: '0.5.7'
+            version: '0.5.13'
             // settings: {
             //  optimizer: {
             //    enabled: false,
